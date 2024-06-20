424c2510183db4a7ebe9eb62a102657f377a5a51
shubhamrwt07 <shubhamrwt789@gmail.com>
2024-06-20 12:42:43 +0530

    changes

--- docker/app/deploy.sh
+++ docker/app/deploy.sh
@@ -1,5 +1,9 @@
+# Remove all Docker images
 docker rmi -f $(docker images -q)
+
+# Tag the latest images as previous
 docker tag r_server:latest r_server:previous
 docker tag vercel-demo:latest vercel-demo:previous
-# docker tag rowthtechadmin:latest rowthtechadmin:previous
+
+# Build and run the containers
 docker-compose up -d --build
