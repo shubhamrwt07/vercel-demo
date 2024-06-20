361225f67e686d650c8fa43a5a1b4862d94bf6eb
shubhamrwt07 <shubhamrwt789@gmail.com>
2024-06-20 13:32:02 +0530

    rathi

--- docker/app/deploy.sh
+++ docker/app/deploy.sh
@@ -2,8 +2,8 @@
 docker rmi -f $(docker images -q)
 
 # Tag the latest images as previous
+docker tag vercel:latest vercel:previous
 docker tag r_server:latest r_server:previous
-docker tag vercel-demo:latest vercel-demo:previous
 
 # Build and run the containers
 docker-compose up -d --build
